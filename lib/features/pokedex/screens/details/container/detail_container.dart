1b189d3d87436d800764cf90e62b57feccf07a73
Marcelo Martins L <cehlomartins@gmail.com>
2022-02-10 15:50:06 -0300

    3° Commit
    Continuação da aula com implementação da pagina de detalhes e atualização no código.

--- lib/features/pokedex/screens/details/container/detail_container.dart
+++ lib/features/pokedex/screens/details/container/detail_container.dart
@@ -7,20 +7,42 @@ import 'package:pokedex2flutter/common/widgets/po_loading.dart';
 import 'package:pokedex2flutter/features/pokedex/screens/details/pages/detail_page.dart';
 
 class DetailArguments {
-  DetailArguments({required this.pokemon});
+  DetailArguments({this.index = 0, required this.pokemon});
   final Pokemon pokemon;
+  final int? index;
 }
 
-class DetailContainer extends StatelessWidget {
-  const DetailContainer(
-      {Key? key, required this.repository, required this.arguments})
-      : super(key: key);
+class DetailContainer extends StatefulWidget {
+  const DetailContainer({
+    Key? key,
+    required this.repository,
+    required this.arguments,
+    required this.onBack,
+  }) : super(key: key);
   final IPokemonRepository repository;
   final DetailArguments arguments;
+  final VoidCallback onBack;
+
+  @override
+  State<DetailContainer> createState() => _DetailContainerState();
+}
+
+class _DetailContainerState extends State<DetailContainer> {
+  late PageController _controller;
+  late Future<List<Pokemon>> _future;
+  Pokemon? _pokemon;
+  @override
+  void initState() {
+    _controller = PageController(
+        viewportFraction: 0.5, initialPage: widget.arguments.index!);
+    _future = widget.repository.getAllPokemons();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<List<Pokemon>>(
-        future: repository.getAllPokemons(),
+        future: _future,
         builder: (context, snaphot) {
           if (snaphot.connectionState == ConnectionState.waiting) {
             return const PoLoading();
@@ -28,7 +50,21 @@ class DetailContainer extends StatelessWidget {
 
           if (snaphot.connectionState == ConnectionState.done &&
               snaphot.hasData) {
-            return DetailPage(pokemon: arguments.pokemon, list: snaphot.data!);
+            // ignore: prefer_conditional_assignment
+            if (_pokemon == null) {
+              _pokemon = widget.arguments.pokemon;
+            }
+            return DetailPage(
+              pokemon: _pokemon!,
+              list: snaphot.data!,
+              onBack: widget.onBack,
+              controller: _controller,
+              onChangePokemon: (Pokemon value) {
+                setState(() {
+                  _pokemon = value;
+                });
+              },
+            );
           }
 
           if (snaphot.hasError) {
